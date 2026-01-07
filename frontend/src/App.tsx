867a696d934b5d70344a260643106a43415c0467
kshimate <amaymishra100@gmail.com>
2026-01-08 01:27:29 +0530

    made Home Page, Need to style It tho

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
 import './App.css'
-import { useState } from 'react'
-// import Home from './pages/Home'
-import Slam from './pages/slamPg/Slam'
 import Auth from './pages/auth/Auth'
+import Home from './pages/Home'
+import Slam from './pages/slamPg/Slam'
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home') // 'home' or 'slam'
 
   return (
     <>
-      {/* <Slam/> */}
+      <Slam/>
       <Auth/>
+      <Home/>
     </>
   )
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import {useState, useEffect } from 'react'
 
 function Home() {
+  // interface User {
+  //   _id: string
+  //   nickName: string
+  // }
+
+  const [users, setUsers] = useState<any[]>([])
+
+  useEffect(() => {
+    axios.get('/api/users')
+    .then((response) => {
+      setUsers(response.data)
+    })
+    .catch((error) => {
+      console.log('found Error: ', error);
+    })
+  }, [])
+
+
   return (
-    <div>Home</div>
+    <div className='bg-gray-800 text-white'>
+      <h1> Users </h1>
+      <p> numberOfUsers: {users.length || 0} </p>
+      {
+        users?.map((user: any, idx: number) => (
+          <div key={user._id}>
+            <h3>{user.nickName}</h3>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
